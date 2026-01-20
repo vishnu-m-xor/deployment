d008f1be5a980e34f22ade32da9aae6264187ec8
vishnu <Katta.Vishnuvardan@xoriant.com>
2026-01-20 17:01:46 +0000

    Test jenkins auto-build

--- src/main/java/com/assignment/AssignmentApplication.java
+++ src/main/java/com/assignment/AssignmentApplication.java
@@ -2,7 +2,7 @@ package com.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Testing Jenkins auto-build
 @SpringBootApplication
 public class AssignmentApplication {
 
